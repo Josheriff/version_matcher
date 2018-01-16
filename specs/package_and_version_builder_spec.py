e962a512d9bbdc3c00158a43dbf2945560c69cd5
Jose <jmgallego@alea-soluciones.com>
2018-01-16 15:52:21 +0100

    PackageAndVersionBuilder Done

--- specs/package_and_version_builder_spec.py
+++ specs/package_and_version_builder_spec.py
@@ -1,10 +1,54 @@
 # -*- coding: utf-8 -*-
 
-from expects import expect, be_true
+from expects import expect, equal
 
+#--------------------------
+def package_and_version_builder(input_line):
+    if "==" not in input_line:
+        return None
+    package, version = input_line.split('==')
+    return {package: version}
+#--------------------------
 
 with describe('PackageAndVersionBuilder'):
-    with it('FOO'):
-        expect(True).to(be_true)
+    with context('when line contains package name and version, splitted by ==  (HappyPath)'):
+        with it('returns a dictonary with package_name as key and version as value'):
+            input_line = 'mamba==0.9.2'
+            another_input_line = 'expect==42'
 
+            result =  package_and_version_builder(input_line)
+            result_2 = package_and_version_builder(another_input_line)
+
+            expected_result = {'mamba': '0.9.2'}
+            expect(expected_result).to(equal(result))
+            expected_result = {'expect': '42'}
+            expect(expected_result).to(equal(result_2))
+
+
+    with context('when line contains package name and no version. Not splitted by =='):
+        with it('returns None'):
+            input_line = 'mamba'
+
+            result =  package_and_version_builder(input_line)
+
+            expected_result = None
+            expect(expected_result).to(equal(result))
+
+    with context('when is an empty line'):
+        with it('returns None'):
+            input_line = ''
+
+            result =  package_and_version_builder(input_line)
+
+            expected_result = None
+            expect(expected_result).to(equal(result))
+
+    with context('when line contains a github reference'):
+        with it('returns None'):
+            input_line = '-e git+https://github.com/aleasoluciones/simpledatamigrate.git#egg=simpledatamigrate'
+
+            result =  package_and_version_builder(input_line)
+
+            expected_result = None
+            expect(expected_result).to(equal(result))
 
