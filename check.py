cd86076db5884fe76acd30c27a0169cb0f7a582e
Jose <jmgallego@alea-soluciones.com>
2018-02-06 15:11:47 +0100

    added error general error handling and visual xxx or ok in the report

--- check.py
+++ check.py
@@ -6,18 +6,25 @@ from version_checker.package_version_retriever import PypiPackageVersionRetrieve
 import sys
 import requests
 
-fname= sys.argv[1]
 
-with open(fname) as f:
-    lines = f.readlines()
-lines = [x.strip() for x in lines]
+try:
+    fname= sys.argv[1]
 
-for line in lines:
-    local_package = package_and_version_builder(line)
+    with open(fname) as f:
+        lines = f.readlines()
+    lines = [x.strip() for x in lines]
 
-    pypi_client = PypiClient(requests)
-    retriever = PypiPackageVersionRetriever(pypi_client)
-    result = retriever.retrieve_version(local_package['name'])
+    for line in lines:
+        local_package = package_and_version_builder(line)
 
-    print local_package['name'],' local version || up to date version ', local_package['version'], ' || ' , result
+        pypi_client = PypiClient(requests)
+        retriever = PypiPackageVersionRetriever(pypi_client)
+        result = retriever.retrieve_version(local_package['name'])
+
+        if local_package['version'] == result:
+            print local_package['name'],' local version || up to date version ', local_package['version'], ' || ' , result , 'OK'
+        else:
+            print local_package['name'],' local version || up to date version ', local_package['version'], ' || ' , result , 'XXX'
+except:
+    print "ERROR, has especificado la ruta del archivo?, ejemplo: python check.py dev-dependencies.txt"
 
