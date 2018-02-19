e1da3ad0e227a42a1b8daed5af76fd947c241764
Jose <jmgallego@alea-soluciones.com>
2018-02-19 16:07:58 +0100

    Prefer all the comparations in the same class

--- specs/version_comparator.py
+++ specs/version_comparator.py
@@ -3,25 +3,6 @@
 from expects import expect, equal
 
 #######
-
-class VersionComparator(object):
-
-    def __init__(self, line_normalizer, version_retriever):
-        self.line_normalizer = line_normalizer
-        self.version_retriever = version_retriever
-
-    def apply(self, line):
-
-        local_package = self.line_normalizer.apply(line)
-        cloud_package_version = self.version_retriever.retrieve_version(local_package['package_name'])
-        if local_package['version'] == cloud_package_version:
-            return True
-        return False
-        # recupero las lineas / del colaborador
-        # por cada línea
-        # >>>> normalizo
-        # >>>>> comparo
-
 class Filereader(object):
 
     def open_and_read(self,filename):
@@ -32,15 +13,33 @@ class Filereader(object):
 
         return lines
 
+class VersionComparator(object):
+
+    def __init__(self, line_normalizer, version_retriever):
+        self.line_normalizer = line_normalizer
+        self.version_retriever = version_retriever
+
+    def apply(self, line):
+
+
 class CompareFullFile(object):
 
-    def __init__(self, lines, version_comparator):
+    def __init__(self, lines, line_normalizer, version_retriever):
         self.lines = lines
-        self.version_comparator = version_comparator
+        self.line_normalizer = line_normalizer
+        self.version_retriever = version_retriever
 
-    def full_compare(self,self.lines):
+    def full_compare(self, self.lines):
         for line in self.lines:
-            self.version_comparator.apply(line)
+            self._version_line_comparator(line)
+
+    def _version_line_comparator(self, line):
+        local_package = self.line_normalizer.apply(line)
+        cloud_package_version = self.version_retriever.retrieve_version(local_package['package_name'])
+        if local_package['version'] == cloud_package_version:
+            return True
+        return False
+
 
 #######
 INPUT_LINE = 'mamba==0.9.2'
