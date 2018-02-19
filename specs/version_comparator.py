33b9c51a368596a54acc14aa1c8a347acb907a27
Jose <jmgallego@alea-soluciones.com>
2018-02-19 11:09:35 +0100

    THE MORE TDD AND OOP STUDY the more I change this :S

--- specs/version_comparator.py
+++ specs/version_comparator.py
@@ -11,7 +11,12 @@ class VersionComparator(object):
         self.version_retriever = version_retriever
 
     def apply(self, requirement_file_name):
-        pass
+        
+        local_package = self.line_normalizer.apply(input_line)
+        cloud_package_version = self.version_retriever.retrieve_version(local_package['package_name'])
+        if local_package['version'] == cloud_package_version:
+            return True
+        return False
         # recupero las lineas / del colaborador
         # por cada línea
         # >>>> normalizo
@@ -22,13 +27,24 @@ class VersionComparator(object):
 
 
 #######
-NORMALIZER_RESULT = {'name': 'mamba', 'version': '0.9.2'}
-USING_UPTODATED = '0.9.2'
+INPUT_LINE = 'mamba==0.9.2'
+USING_UPTODATE = '0.9.2'
 USING_OUTDATED = '0.9.3'
+
+PYPIJSON =
 
 with describe('comparasion between server and local'):
     with it('both using the same version'):
-        comparator = VersionComparator()
+
+        http_client = Stub()
+        pypiclient = Stub(PypiClient(http_client))
+        version_retriever = Stub(PypiPackageVersionRetriever(pypiclient))
+        when(version_retriever).apply(ANY_PARAMETER).returns(USING_UPTODATE)
+        line_normalizer = RequirementLineNormalizer()
+
+        comparator = VersionComparator(line_normalizer, version_retriever, package_name)
+
+
 
 
 
